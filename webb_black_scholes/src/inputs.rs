f42204d1b6310b99cd604562d292b55834c19a26
Jeremiah Webb <illusion173@hotmail.com>
2023-05-12 13:30:05 -0400

    users can create a basic input for an option

--- webb_black_scholes/src/inputs.rs
+++ webb_black_scholes/src/inputs.rs
@@ -10,8 +10,45 @@ pub enum OptionType {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Inputs {
-    // inputs here
+    /// Type of Option (Call or Put)
+    pub option_type: OptionType,
+    /// Stock Price
+    pub s: f32,
+    /// Strike Price of Option
+    pub k: f32,
+    /// Option Price
+    pub p: Option<f32>,
+    /// Risk-free Rate
+    pub r: f32,
+    /// Dividend Yield
+    pub q: f32,
+    /// Time to Maturity (years)
+    pub t: f32,
+    /// Volatility
+    pub sigma: Option<f32>,
 }
 
-// methods for inputs
-impl Inputs {}
+/// Methods for calculating the price, greeks, and implied volatility of an option.
+impl Inputs {
+    pub fn new(
+        option_type: OptionType,
+        s: f32,
+        k: f32,
+        p: Option<f32>,
+        r: f32,
+        q: f32,
+        t: f32,
+        sigma: Option<f32>,
+    ) -> Self {
+        Self {
+            option_type,
+            s,
+            k,
+            p,
+            r,
+            q,
+            t,
+            sigma,
+        }
+    }
+}
